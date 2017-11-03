9b71a01eac524e49e2f6e6ee3c1227ca07b32b7b
miguel terceros <shekellkan31@gmail.com>
2017-11-02 20:08:12 -0400

    errorFix capa8

--- sisVentas/resources/views/layouts/admin.blade.php
+++ sisVentas/resources/views/layouts/admin.blade.php
@@ -7,16 +7,16 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css)}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('css/font-awesome.css)}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css)}}">
+    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{asset('css/_all-skins.min.css)}}">
-    <link rel="apple-touch-icon" href="{{asset{{asset('('img/apple-touch-icon.png)}}">
-    <link rel="shortcut icon" href="{{asset('img/favicon.ico)}}">
+    <link rel="stylesheet" href="{{asset('css/_all-skins.min.css')}}">
+    <link rel="apple-touch-icon" href="{{asset('img/apple-touch-icon.png')}}">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
 
   </head>
   <body class="hold-transition skin-blue sidebar-mini">
